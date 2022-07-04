29f7f25716c33f719475b5106a0fbf2d36cb16bf
ZPAVelocity <velocity120@outlook.com>
2022-07-04 22:26:04 +0800

    service common config swagger

--- bs/service/common/src/main/java/com/example/common/config/Swagger2Config.java
+++ bs/service/common/src/main/java/com/example/common/config/Swagger2Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +27,7 @@ public class Swagger2Config {
                 .title("bs service")
                 .description("a spring cloud rtc project")
                 .termsOfServiceUrl("")
-                .contact("wzklhk")
+                .contact(new Contact("wzklhk", "https://github.com/wzklhk", "velocity120@outlook.com"))
                 .version("0.0.1")
                 .build();
     }
