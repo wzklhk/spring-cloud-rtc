3e8598afabf14f3e7cbaf857d64d303c72f4d102
ZPAVelocity <velocity120@outlook.com>
2022-07-04 20:59:38 +0800

    service common 通用接口

---
+++ bs/service/common/src/main/java/com/example/common/api/service/CommonService.java
@@ -0,0 +1,28 @@
+package com.example.common.api.service;
+
+import com.example.common.api.ErrorCodeEnum;
+import com.example.common.api.PageCommon;
+import com.example.common.api.PageQuery;
+
+import java.util.List;
+
+/**
+ * 通用 Service
+ *
+ * @param <V>  实体类Vo
+ * @param <E>  实体类
+ * @param <ID> id主键类型
+ */
+public interface CommonService<V, E, ID> {
+
+    V getById(ID id);
+
+    List<V> list(V entityVo);
+
+    PageCommon<V> listByPage(PageQuery query);
+
+    V saveOrUpdate(V entityVo);
+
+    ErrorCodeEnum deleteById(ID id);
+
+}
