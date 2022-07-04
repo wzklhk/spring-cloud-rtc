2afb46206d413e95e516a7a234dfcdd535dcc87a
ZPAVelocity <velocity120@outlook.com>
2022-07-04 18:00:35 +0800

    bs rtc ws controller

--- bs/service/rtc/src/main/java/com/example/rtc/ws/controller/WebsocketController.java
+++ bs/service/rtc/src/main/java/com/example/rtc/ws/controller/WebsocketController.java
@@ -1,9 +1,11 @@
 package com.example.rtc.ws.controller;
 
+import com.example.common.api.ResultInfo;
+import com.example.common.pojo.message.entity.Message;
+import com.example.rtc.ws.service.WebsocketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ws")
@@ -15,10 +17,27 @@ public class WebsocketController {
     @Value("${server.port}")
     private Integer port;
 
-    @GetMapping("getWsUrl")
-    public String getWsUrl() {
-        String url = "wss://" + ip + ":" + port;
-        System.out.println(url);
-        return url;
+    @Autowired
+    private WebsocketService websocketService;
+
+    @GetMapping("/getUrl")
+    public ResultInfo getUrl() {
+        String url = "ws://" + ip + ":" + port;
+        return ResultInfo.ok(url);
+    }
+
+    @PostMapping("/unicast")
+    public void unicast(@RequestBody Message message) {
+        websocketService.unicastMessage(message.getReceiveUser(), message.getData());
+    }
+
+    @PostMapping("/multicast")
+    public void multicast(@RequestBody Message message) {
+        websocketService.multicastMessage(message.getReceiveUsers(), message.getData());
+    }
+
+    @PostMapping("/broadcast")
+    public void broadcast(@RequestBody Message message) {
+        websocketService.broadcastMessage(message.getData());
     }
 }
