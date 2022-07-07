34edcc44c934957ec5cbe880bc1b1cc78c5347bf
ZPAVelocity <velocity120@outlook.com>
2022-07-07 17:47:01 +0800

    server common 添加跨域

---
+++ bs/service/common/src/main/java/com/example/common/config/WebMvcCorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true)
+                .maxAge(36000);
+    }
+}
