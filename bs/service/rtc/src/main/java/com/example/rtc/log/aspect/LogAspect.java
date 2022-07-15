0d9531e2cf4ddfad426d435d689e2a3cf782dbc0
ZPAVelcoity <velocity120@outlook.com>
2022-07-15 10:49:48 +0800

    bs rtc add log aspect

---
+++ bs/service/rtc/src/main/java/com/example/rtc/log/aspect/LogAspect.java
@@ -0,0 +1,22 @@
+package com.example.rtc.log.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+
+    @Pointcut("execution(* com.example.rtc.ws.controller.WebsocketController.*(..))")
+    public void wsControllerPointcut() {
+    }
+
+    @Before(value = "wsControllerPointcut()")
+    public void beforeWsPointcut() {
+        log.info("test aspect");
+    }
+}
